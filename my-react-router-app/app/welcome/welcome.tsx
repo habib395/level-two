dabcf7d3db37a856f266e0a7350b9502b039b29c
Habibur Rahman <md.habiburrahmanjwd@gmail.com>
2025-04-03 21:05:54 +0600

    welcome to next level journry

---
+++ my-react-router-app/app/welcome/welcome.tsx
@@ -0,0 +1,16 @@
+import Contact from "~/routes/contact";
+import logoDark from "./logo-dark.svg";
+import logoLight from "./logo-light.svg";
+
+export function Welcome() {
+  return (
+    <main>
+     <h2>this is todo app.</h2>
+     <p>this is me.</p>
+     <div className="flex item-center justify-center">
+     <Contact></Contact>
+     </div>
+    </main>
+  );
+}
+
